0a58dd67bd470da7652d87d78ebc5923e8aa54f8
atharva-k-pccoe <134262316+atharva-k-pccoe@users.noreply.github.com>
2023-06-03 18:27:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
-
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 // export const instance = new Razorpay({
 //   key_id: process.env.RAZORPAY_API_KEY,
